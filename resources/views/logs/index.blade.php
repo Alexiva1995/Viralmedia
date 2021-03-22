d9c7222447258ef3252e7ceb7e0a0b9265ccfe8d
Alexiva1995 <alexisjoseva95@valdusoft.com>
2021-03-22 14:10:53 -0400

    transacctions logs

--- resources/views/logs/index.blade.php
+++ resources/views/logs/index.blade.php
@@ -20,20 +20,35 @@
                                     <th>Monto (Comision Incluida)</th>
                                     <th>Estado</th>
                                     <th>Fecha de Creación</th>
-                                    <th>Acción</th>
                                 </tr>
                             </thead>
                             <tbody>
+                                @foreach ($logs as $item)
                                 <tr class="text-center">
-                                    <td> 1</td>
-                                    <td> admin</td>
-                                    <td>Venezuela</td>                                    
-                                    <td>12/28/2020</td>
-                                    <td> Retiro</td>
-                                    <td> 50</td>
-                                    <td> 50</td>
-                                    <td> 50</td>
+                                    <td>{{$item->id}}</td>
+                                    <td>{{$item->getUser->fullname}}</td>
+                                    <td>{{$item->id_transacion}}</td>
+                                    @if ($item->metodo_pago == 'Stripe')
+                                    <td><img src="{{asset('assets/img/sistema/stripe1.png')}}" height="40" width="80"></td>
+                                    @elseif($item->metodo_pago == 'Skrill')
+                                    <td><img src="{{asset('assets/img/sistema/skrill1.png')}}" height="20" width="60"></td>
+                                    @elseif($item->metodo_pago == 'Payu')
+                                    <td><img src="{{asset('assets/img/sistema/payu1.png')}}" height="50" width="90"></td>
+                                    @elseif($item->metodo_pago == 'Coinbase')
+                                    <td><img src="{{asset('assets/img/sistema/coinbase1.png')}}" height="47" width="100"></td>
+                                    @endif
+                                    
+                                    <td>{{$item->saldo}}</td>
+                                    @if ($item->estado == '0')
+                                    <td> <a class=" btn btn-info text-white text-bold-600">Esperando</a></td>
+                                    @elseif($item->estado == '1')
+                                    <td> <a class=" btn btn-success text-white text-bold-600">Aprobado</a></td>
+                                    @elseif($item->estado == '2')
+                                    <td> <a class=" btn btn-danger text-white text-bold-600">Cancelado</a></td>
+                                    @endif
+                                    <td>{{$item->fecha_creacion}}</td>
                                 </tr>
+                                @endforeach
                                
                             </tbody>
                         </table>
